24b88d62966362f0ac584a49e6cefe0cbfa8357d
Rahulgit3D2Y <rc2504000@gmail.com>
2023-08-25 19:13:54 +0530

    1

---
+++ src/components/Account/index.js
@@ -0,0 +1,9 @@
+import { Typography } from '@mui/material'
+ 
+const Account = () => {
+  return (
+    <Typography>account</Typography>
+  )
+}
+
+export default Account;
---
+++ src/components/Home/index.js
@@ -0,0 +1,10 @@
+import { Typography } from '@mui/material'
+import React from 'react'
+import { BrowserRouter } from 'react-router-dom'
+ const Home = () => {
+  return (
+    <Typography>home</Typography>
+  )
+}
+
+export default Home;
